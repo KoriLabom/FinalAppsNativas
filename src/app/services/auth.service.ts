a1341ee69dcae26c05b5faff983046c641af6659
Mateo Labombarda <ilovepll2005@gmail.com>
2025-11-05 12:34:29 -0300

    login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { LoginData } from '../interfaces/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
-  
+  router = inject(Router);
+  token : null|string = localStorage.getItem("token");
+
+    /** Autentica al usuario en el back y nos devuelve el token */
+  async login(loginData: LoginData){
+    const res = await fetch("https://restaurant-api.somee.com/api/Authentication/login",
+      {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(loginData)
+      }
+    )
+    if(res.ok){
+      this.token = await res.text()
+      localStorage.setItem("token",this.token);
+      this.router.navigate(["/"])
+    }
+  }
+
+  /** Cierra sesión */
+  logout(){
+    this.token = null;
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
+  }
 }
